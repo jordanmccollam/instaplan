198910e9790f62898ecdfbb4ccef9d1a312ee039
Jordan McCollam <jordy.mccollam@gmail.com>
2021-04-12 14:24:15 -0400

    Added hoverable icons to menu

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -8,6 +8,9 @@ import './_menu.scss';
 
 const logger = "Menu:: ";
 
+const iconSize = '2vw';
+const iconColor = 'white';
+
 const Menu = (props) => {
   let classes = {
 		[`menu`]: true
@@ -17,7 +20,12 @@ const Menu = (props) => {
     <Card className={`${props.className} ${classnames(classes)}`}>
       <Logo color="white" break size={'2.3vw'} />
 
-      
+      <div className="mb-5" />
+
+      <div className="menu-navigation">
+        <Icon size={iconSize} color={iconColor} className="menu-item" />
+        <Icon size={iconSize} color={iconColor} className="menu-item" />
+      </div>
     </Card>
   )
 }
