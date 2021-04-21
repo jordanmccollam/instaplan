a1a79cab596e031e157e74872e52244501ed5bfa
Jordan McCollam <jordy.mccollam@gmail.com>
2021-04-21 15:16:36 -0400

    Testing something

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,8 @@ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const audience = process.env.REACT_APP_AUDIENCE;
 
+console.log('TEST', {domain, clientId, audience});
+
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
